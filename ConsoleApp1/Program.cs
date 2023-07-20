c7b26d28958a40b57021821b9fb3a99ea0b3669a
Andrey Budaev <andrey.budaev@acumatica.com>
2023-07-20 14:45:57 -0400

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            for (int i = 0; i < 1000; i++) {
+
+                if (counter % 100 == 0)
+                    Console.WriteLine($"files analyzed - {counter}");
+
+                counter++;
+            }
+        }
+    }
+}
